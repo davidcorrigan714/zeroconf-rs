eb0cf0d3fecb68947cb71f5954e064816d0bbccd
davidcorrigan714 <davidcorrigan714@gmail.com>
2024-01-14 12:43:25 -0600

    try that

---
+++ scripts/verify.ps1
@@ -0,0 +1,4 @@
+pwsh ./scripts/buildall.ps1
+pwsh ./scripts/checkfmt.ps1
+pwsh ./scripts/lintall.ps1
+cargo test
--- zeroconf/src/macros.rs
+++ zeroconf/src/macros.rs
@@ -1,3 +1,5 @@
+#[cfg_attr(feature = "cargo-clippy", allow(useless_ptr_null_checks))]
+
 macro_rules! assert_not_null {
     ($ptr:expr) => {
         assert!(!$ptr.is_null(), "expected non-null value");
